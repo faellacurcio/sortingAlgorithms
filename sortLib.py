f5982b5e6dfd5da4f04787938a8ac1dd155a12f6
Rafael Acurcio <rafael.acurcio@gmail.com>
2018-01-01 22:01:20 -0300

    Selection implemented

--- sortLib.py
+++ sortLib.py
@@ -19,6 +19,12 @@ class sorter():
 		else:
 			return x < y
 
+	def selectionCompare(x, y, order):
+		if(order == -1):
+			return x > y
+		else:
+			return x < y
+
 	def swapElements(array, index1, index2):
 		aux = array[index1]
 		array[index1] = array[index2]
@@ -27,8 +33,8 @@ class sorter():
 	def bouble(self, array, order = 1, plot = False):
 		logging.info("given array:")
 		logging.info(array)
-		logging.info("generated array:")
-		logging.info([i for i in range(1, len(array) + 1)])
+		if(order not in [1, -1]):
+			logging.warning("Order value not valid (1, -1)")
 
 		for loop in range(1, len(array)):
 			for iterator in range(0, len(array) - loop):
@@ -39,9 +45,16 @@ class sorter():
 						self.ax.bar([i for i in range(1, len(array) + 1)], array, self.width)
 						plt.pause(0.01 / len(array))
 
+		logging.info("Sorted array:")
+		logging.info(array)
 		return array
 
 	def insertion(self, array, order = 1, plot = True):
+		logging.info("given array:")
+		logging.info(array)
+		if(order not in [1, -1]):
+			logging.warning("Order value not valid (1, -1)")
+
 		elementIndex = 1
 		while(elementIndex < len(array)):
 			insertedElemntIndex = elementIndex
@@ -53,21 +66,46 @@ class sorter():
 					self.ax.bar([i for i in range(1, len(array) + 1)], array, self.width)
 					plt.pause(0.01 / len(array))
 			elementIndex = elementIndex + 1
+		logging.info("Sorted array:")
+		logging.info(array)
+		return array
+
+	def selection(self, array, order = 1, plot = True):
+		# for i in range(len(array)):
+		for j in range(len(array)):
+			sorter.swapElements(
+				array, 
+				j, 
+				array[j:].index(min(array[j:])) + j
+			) 
+
+			if(plot):
+					self.ax.clear()
+					self.ax.bar([i for i in range(1, len(array) + 1)], array, self.width)
+					plt.pause(0.01 / len(array))
+
 		return array
 
 
 sort = sorter()
 
 randomArray = list(np.random.randint(100, size= 20))
+print("Random array:")
+print(randomArray)
+print("----------------")
 
 boubleArray = randomArray.copy()
-print("Bouble:")
-print(sort.bouble(boubleArray, order = -1, plot = True))
-
 insertionArray = randomArray.copy()
+selectionArray = randomArray.copy()
+
+print("Bouble:")
+print(sort.bouble(boubleArray, order = -1, plot = False))
 
 print("Insertion:")
-print(sort.insertion(randomArray))
+print(sort.insertion(randomArray, order = 1, plot = False))
+
+print("Insertion:")
+print(sort.selection(selectionArray, order = 1, plot = True))
 
 # -------------------
 # timeit experiments:
